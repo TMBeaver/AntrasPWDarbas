2795fcaf086c7185c560a4ca4d50770c75cfb435
TMBeaver <144656698+TMBeaver@users.noreply.github.com>
2023-12-11 17:48:02 +0200

    Update ExampleInstrumentedTest.java
    testing added

--- src/androidTest/java/com/example/task2/ExampleInstrumentedTest.java
+++ src/androidTest/java/com/example/task2/ExampleInstrumentedTest.java
@@ -1,26 +1,49 @@
 package com.example.task2;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    // This rule launches the MainActivity before each test
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.task2", appContext.getPackageName());
+    public void testEditTextInput() {
+        // Type text into the EditText
+        Espresso.onView(ViewMatchers.withId(R.id.editText))
+                .perform(ViewActions.typeText("Hello"), ViewActions.closeSoftKeyboard());
+
+        // Click the count button
+        Espresso.onView(ViewMatchers.withId(R.id.countButton)).perform(ViewActions.click());
+
+        // Check if the resultTextView displays the correct result
+        Espresso.onView(ViewMatchers.withId(R.id.resultTextView))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Result: 1")));
+    }
+
+    @Test
+    public void testSpinnerSelection() {
+        // Select "Characters" from the spinner
+        Espresso.onView(ViewMatchers.withId(R.id.spinner))
+                .perform(ViewActions.click());
+        Espresso.onData(ViewMatchers.is("Characters")).perform(ViewActions.click());
+
+        // Click the count button
+        Espresso.onView(ViewMatchers.withId(R.id.countButton)).perform(ViewActions.click());
+
+        // Check if the resultTextView displays the correct result
+        Espresso.onView(ViewMatchers.withId(R.id.resultTextView))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Result: 0")));
     }
 }
