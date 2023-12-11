503039b0f1d805c8b08999114f116fa1436bae7d
TMBeaver <144656698+TMBeaver@users.noreply.github.com>
2023-12-11 17:48:25 +0200

    Update ExampleUnitTest.java
    testing added

--- src/test/java/com/example/task2/ExampleUnitTest.java
+++ src/test/java/com/example/task2/ExampleUnitTest.java
@@ -9,9 +9,37 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
+
+
+        @Test
+        public void countWords_validInput_returnsCorrectCount() {
+            String inputText = "This is a sample sentence.";
+            int expectedCount = 5; // The sentence has 5 words
+            int actualCount = WordCharacterCounter.countWords(inputText);
+            assertEquals(expectedCount, actualCount);
+        }
+
+        @Test
+        public void countWords_emptyInput_returnsZero() {
+            String inputText = "";
+            int expectedCount = 0; // Empty string should have 0 words
+            int actualCount = WordCharacterCounter.countWords(inputText);
+            assertEquals(expectedCount, actualCount);
+        }
+        @Test
+        public void countCharacters_validInput_returnsCorrectCount() {
+            String inputText = "Hello, World!";
+            int expectedCount = 13; // The string has 13 characters
+            int actualCount = WordCharacterCounter.countCharacters(inputText);
+            assertEquals(expectedCount, actualCount);
+        }
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void countCharacters_emptyInput_returnsZero() {
+        String inputText = "";
+        int expectedCount = 0; // Empty string should have 0 characters
+        int actualCount = WordCharacterCounter.countCharacters(inputText);
+        assertEquals(expectedCount, actualCount);
     }
-}
+    }
